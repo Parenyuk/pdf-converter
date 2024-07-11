c3ab7c0af54cd2bab6ddd2ba10aef49835472679
Parenyuk <ParenVic@i.ua>
2024-07-11 21:28:19 +0300

    PDF-002, added initial converter

---
+++ src/app/(home)/actions/index.ts
@@ -0,0 +1,29 @@
+"use server"
+
+export async function convertTextToPdf(text: string) {
+
+    const url = `http://95.217.134.12:4010/create-pdf?apiKey=${process.env.API_KEY}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text })
+        });
+
+        if (response.ok) {
+            const res = await response.blob();
+            const blob = new Blob([res], { type: 'application/pdf' });
+            const UintArr = new Uint8Array(await blob.arrayBuffer());
+            return UintArr;
+
+        } else {
+            throw new Error(`Failed to create PDF: ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
--- src/app/(home)/components/PDFConverter/PDFConverter.tsx
+++ src/app/(home)/components/PDFConverter/PDFConverter.tsx
@@ -1,15 +1,55 @@
+"use client"
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { useState } from "react";
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import {convertTextToPdf} from "@/app/(home)/actions";
+import packageJson from '../../../../../package.json';
 
-
-type ComponentType = {
-
-}
+type ComponentType = {}
 
 export const PdfConverter = ({}: ComponentType) => {
-    return (<div className={`flex flex-col items-center`}>
-        <h1 className={'text-xl'}>Pdf Converter</h1>
-        <textarea className="mx-[0] my-[8px] px-[16px] py-[12px] rounded-[8px] min-h-[250px] min-w-[400px] resize-y" />
-        <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg 
-        transition-transform transform focus:outline-none 
-        focus:ring-blue-300 focus:ring-opacity-50`}>Convert to PDF</button>
-    </div>)
+    const pdfjsVersion = packageJson.devDependencies['pdfjs-dist'];
+
+    const [text, setText] = useState('');
+    const [pdfUrl, setPdfUrl] = useState<Uint8Array>(null);
+
+
+    const handleConvertToPdf = async () => {
+        try {
+            const pdfUrl = await convertTextToPdf(text);
+            console.log('pdfUrl', pdfUrl);
+            setPdfUrl(pdfUrl);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-xl">Pdf Converter</h1>
+            <textarea
+                className="mx-0 my-2 px-4 py-3 rounded min-h-[250px] min-w-[400px] resize-y"
+                rows="10"
+                placeholder="Enter the text"
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg transition-transform transform focus:outline-none focus:ring-blue-300 focus:ring-opacity-50"
+                onClick={handleConvertToPdf}
+            >
+                Convert to PDF
+            </button>
+
+            <div className="h-screen w-screen">
+                {pdfUrl && (
+                        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}>
+                        <Viewer
+                            fileUrl={pdfUrl}
+                            style={{ border: '1px solid rgba(0, 0, 0, 0.3)', height: '350px' }}
+                        />
+                    </Worker>
+                )}
+            </div>
+        </div>
+    );
 };
