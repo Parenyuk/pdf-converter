e90631731cdf1ad2bf97c5c4b84d7d355513562a
Parenyuk <ParenVic@i.ua>
2024-07-11 12:13:32 +0300

    PDF-002, added initial home page

---
+++ src/app/(home)/components/PDFConverter/PDFConverter.tsx
@@ -0,0 +1,9 @@
+
+
+type ComponentType = {
+
+}
+
+export const PdfConverter = ({}: ComponentType) => {
+    return (<div>Pdf Converter111 </div>)
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700", "900"]
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -10,13 +14,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+      <body className={roboto.className}>
+        {children}
+      </body>
+      </html>
   );
 }
